3f45b3546a3ff65f93b70f10e867d732e1daf3c4
Luan Vendt <luan.vendt@bitize.com.br>
2023-08-21 13:54:31 -0300

    criando arquivos produtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user/user.controller';
 
 @Module({
   imports: [],
-  controllers: [UserController],
-  providers: [],
 })
 export class AppModule {}
